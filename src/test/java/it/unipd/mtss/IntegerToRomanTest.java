de89dfcd863bce3be35040f966c080e8656d77d0
gmarzola <gmarzola@ad.studenti.math.unipd.it>
2025-05-12 15:15:42 +0200

    Test 500 close #11

--- src/test/java/it/unipd/mtss/IntegerToRomanTest.java
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -45,5 +45,13 @@ public class IntegerToRomanTest {
         assertEquals("LXXX", IntegerToRoman.convert(80));
         assertEquals("XC", IntegerToRoman.convert(90));
         assertEquals("C", IntegerToRoman.convert(100));
+
+        
+        //Test 500
+        assertEquals("CC", IntegerToRoman.convert(200));
+        assertEquals("CCC", IntegerToRoman.convert(300));
+        assertEquals("CD", IntegerToRoman.convert(400));
+        assertEquals("D", IntegerToRoman.convert(500));
+
     }
 }
