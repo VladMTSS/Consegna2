bd83ba932eb15ec80747f0b457ae6c530cdcc3a4
gmarzola <gmarzola@ad.studenti.math.unipd.it>
2025-05-12 14:28:02 +0200

    Test 3 close #5

---
+++ src/test/java/it/unipd/mtss/IntegerToRomanTest.java
@@ -0,0 +1,20 @@
+package it.unipd.mtss;
+
+//////////////////////////////////////////////////////////////////// 
+// VLAD MIHAI PANAIT 	2111949
+// GIOELE MARZOLA 		2113193 
+////////////////////////////////////////////////////////////////////
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class IntegerToRomanTest {
+    @Test
+    public void testConvert() {
+        //Test 3
+        assertEquals("I", IntegerToRoman.convert(1));
+        assertEquals("II", IntegerToRoman.convert(2));
+        assertEquals("III", IntegerToRoman.convert(3));
+    }
+}
---
+++ src/test/java/it/unipd/mtss/RomanPrinterTest.java
@@ -0,0 +1,22 @@
+package it.unipd.mtss;
+
+//////////////////////////////////////////////////////////////////// 
+// VLAD MIHAI PANAIT 	2111949
+// GIOELE MARZOLA 		2113193 
+////////////////////////////////////////////////////////////////////
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThrows;
+
+public class RomanPrinterTest {
+
+    @Test
+    public void testPrint() {
+        // Test 3
+        assertEquals("  _____ \n |_   _|\n   | |  \n   | |  \n  _| |_ \n |_____|\n", RomanPrinter.print(1));
+        assertEquals("  _____   _____ \n |_   _| |_   _|\n   | |     | |  \n   | |     | |  \n  _| |_   _| |_ \n |_____| |_____|\n", RomanPrinter.print(2));
+        assertEquals("  _____   _____   _____ \n |_   _| |_   _| |_   _|\n   | |     | |     | |  \n   | |     | |     | |  \n  _| |_   _| |_   _| |_ \n |_____| |_____| |_____|\n", RomanPrinter.print(3));
+    }
+
+}
